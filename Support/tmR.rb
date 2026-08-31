45f4cd44f13dc7eb7ecc2bae8752551abc15ec84
Shane Becker <veganstraightedge@gmail.com>
2026-08-31 15:37:13 -0700

    Remove the commented-out tm_interactive_input block, which references a library nothing ships

--- Support/tmR.rb
+++ Support/tmR.rb
@@ -47,15 +47,6 @@ def my_popen3(*cmd) # returns [stdin, stdout, strerr, pid]
     STDERR.reopen(pe[1])
     pe[1].close
 
-    # maybe there is a way to support this in future ;)
-    # tm_interactive_input = SUPPORT_LIB + '/tm_interactive_input.dylib'
-    # if (File.exist? tm_interactive_input) 
-    #   dil = ENV['DYLD_INSERT_LIBRARIES']
-    #   ENV['DYLD_INSERT_LIBRARIES'] = (dil) ? "#{tm_interactive_input}:#{dil}" : tm_interactive_input unless (dil =~ /#{tm_interactive_input}/)
-    #   ENV['DYLD_FORCE_FLAT_NAMESPACE'] = "1"
-    #   ENV['TM_INTERACTIVE_INPUT'] = 'AUTO'
-    # end
-    
     exec(*cmd)
   }
 
